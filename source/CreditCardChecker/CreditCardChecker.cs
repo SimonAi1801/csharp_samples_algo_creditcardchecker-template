0e761bc13244e98b8d512bce77bb1a28ee099f0b
Simon Aichmayr <56634821+SimonAi1801@users.noreply.github.com>
2020-03-26 18:41:45 +0100

    implements IsCreditCardValid

--- source/CreditCardChecker/CreditCardChecker.cs
+++ source/CreditCardChecker/CreditCardChecker.cs
@@ -10,29 +10,75 @@ namespace CreditCardChecker
         /// </summary>
         public static bool IsCreditCardValid(string creditCardNumber)
         {
-            throw new NotImplementedException();
+            bool isValid = false;
+            if (creditCardNumber.Length == 16 && CheckString(creditCardNumber))
+            {
+                int evenSum = GetNumberSumOnEvenPosition(creditCardNumber);
+                int oddSum = GetNumberSumOnOddPosition(creditCardNumber);
+                int lastNumber = Convert.ToInt32(creditCardNumber[15] - '0');
+
+                if ((evenSum + oddSum + lastNumber) % 10 == 0)
+                {
+                    isValid = true;
+                }
+            }
+            return isValid;
         }
 
-        /// <summary>
-        /// Berechnet aus der Summe der geraden Stellen (bereits verdoppelt) und
-        /// der Summe der ungeraden Stellen die Checkziffer.
-        /// </summary>
-        private static int CalculateCheckDigit(int oddSum, int evenSum)
+        private static bool CheckString(string creditCardNumber)
         {
-            throw new NotImplementedException();
+            bool isOk = true;
+            for (int i = 0; i < creditCardNumber.Length; i++)
+            {
+                if (char.IsLetter(creditCardNumber[i]))
+                {
+                    isOk = false;
+                }
+            }
+            return isOk;
         }
+
+        private static int GetNumberSumOnOddPosition(string creditCardNumber)
+        {
+            int result = 0;
+            for (int i = 1; i <= 13; i += 2)
+            {
+                int number = Convert.ToInt32(creditCardNumber[i] - '0');
+                result += number;
+            }
+            return result;
+        }
+
+        private static int GetNumberSumOnEvenPosition(string creditCardNumber)
+        {
+            int result = 0;
+            for (int i = 0; i <= 14; i += 2)
+            {
+                int number = Convert.ToInt32(creditCardNumber[i] - '0');
+                int digitSum = CalculateDigitSum(number * 2);
+                result += digitSum;
+            }
+            return result;
+        }
+
 
         /// <summary>
         /// Berechnet die Ziffernsumme einer Zahl.
         /// </summary>
         private static int CalculateDigitSum(int number)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            while (number > 0)
+            {
+                result += (number % 10);
+                number /= 10;
+            }
+            return result;
         }
 
-        private static int ConvertToInt(char ch)
-        {
-            throw new NotImplementedException();
-        }
+        //private static int ConvertToInt(char ch)
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
